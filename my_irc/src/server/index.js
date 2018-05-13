5e25eee7d05d836f8e7583f7a1853ba655087e75
quentin <quentin.guitard@epitech.eu>
2018-05-13 22:26:31 +0200

    DB

--- my_irc/src/server/index.js
+++ my_irc/src/server/index.js
@@ -2,14 +2,28 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
 
 // our localhost port
 const port = 4000;
 
 const app = express();
 
+//Connect to mongodb
+mongoose.connect('mongodb://localhost/myirc');
+mongoose.Promise = global.Promise;
+
 app.use(bodyParser.json());
+// Routes
 app.use("/api", require("./routes/api"));
+// ERROR handling on DB
+app.use(function(err, req, res, next){
+    //console.log(err);
+    res.status(422).send({
+        error: err.message
+    });
+});
 
 const server = http.createServer(app);
 
--- my_irc/src/server/routes/api.js
+++ my_irc/src/server/routes/api.js
@@ -1,41 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const Channel = require('../models/channels');
 
 // Create new channels to DB
-router.post("/channels", function(req, res) {
-    console.log(req.body);
-    res.send({
-        type: "POST",
-        name: req.body.name
-    });
+router.post("/channels", function(req, res, next) {
+    Channel.create(req.body).then(function(channel){
+        res.send(channel);
+    }).catch(next)
 });
 // Read list of channels from DB
-router.get("/channels", function(req, res) {
-    res.send({ type: "GET" });
+router.get("/channels", function(req, res, next) {
+    Channel.find({}).then(function(channel){
+        res.send(channels);
+    })
 });
 // Update channel from DB
-router.put("/channels/:id", function(req, res) {
+router.put("/channels/:id", function(req, res, next) {
     res.send({ type: "PUT" });
 });
 // Delete channel from DB
-router.delete("/channels/:id", function(req, res) {
-    res.send({ type: "DELETE" });
+router.delete("/channels/:id", function(req, res, next) {
+    Channel.findByIdAndRemove({_id:req.params.id}).then(function(channel){
+        res.send(channel);
+    });
 });
 
 // Create new user to DB
-router.post("/users", function(req, res) {
+router.post("/users", function(req, res, next) {
     res.send({ type: "POST" });
 });
 // Read list of Users from DB
-router.get("/users", function(req, res) {
+router.get("/users", function(req, res, next) {
     res.send({ type: "GET" });
 });
 // Update User from DB
-router.put("/users/:id", function(req, res) {
+router.put("/users/:id", function(req, res, next) {
     res.send({ type: "PUT" });
 });
 // Delete User from DB
-router.delete("/users/:id", function(req, res) {
+router.delete("/users/:id", function(req, res, next) {
     res.send({ type: "DELETE" });
 });
 
